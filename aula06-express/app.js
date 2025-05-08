852af3af1eb8018020e5d4e9af244957a29e7f83
Ismael Nascimento <72841488+ismaelnascimento@users.noreply.github.com>
2025-05-08 14:10:34 -0300

    Finalizando atividade da aula 06

--- aula06-express/app.js
+++ aula06-express/app.js
@@ -3,48 +3,44 @@ const app = express();
 const port = 3000;
 
 const pageMiddleware = (req, res, next) => {
-  res.send(`Acessando a página ${req.url}`);
+  console.log(`Page: ${req.url}`)
   next();
 };
 
 app.use(pageMiddleware);
 
 app.get("/", (req, res) => {
-  res.send(`/`);
+  res.send(`Initial page`);
 });
 
 app.get("/about", (req, res) => {
-  res.send(`about`);
+  res.send(`About page`);
 });
 
 app.post("/data", (req, res) => {
-  res.send(`data`);
+  res.send(`Data page to post`);
 });
 
 app.get("/users", (req, res) => {
-  res.send(`Users`);
-});
-
-app.get("/users/signin:id", (req, res) => {
-  if (req.params.id) {
-    res.send(`Bem vindo usuário ${req.params.id}`);
-  } else {
-    res.redirect("/singup");
-  }
+  res.send(`Users page`);
 });
 
 app.get("/users/signin", (req, res) => {
-  res.send(`signin`);
+  res.redirect("/users/signup");
 });
 
-app.get("/users/sinup", (req, res) => {
-  res.send(`singup`);
+app.get("/users/signin/:userid", (req, res) => {
+  res.send(`Welcome user ${req.params.userid}`);
 });
 
-app.use((err, req, res, next) => {
-  res.status(404).send("404 - Página não encontrada");
+app.get("/users/signup", (req, res) => {
+  res.send(`Signup page`);
+});
+
+app.use((err, req, res) => {
+  req.status(404).send("404 - Not Found page");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port: ${port}`);
 });
