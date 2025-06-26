48c2e43a40b83b5a0bf905c97e379f35ae3931dc
Ismael Nascimento <72841488+ismaelnascimento@users.noreply.github.com>
2025-06-26 17:13:49 -0300

    aula 13 update

--- aula13/index.js
+++ aula13/index.js
@@ -3,6 +3,8 @@ const app = express()
 const axios = require("axios")
 const PORT = 3000
 
+app.set('view engine', 'ejs');
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
@@ -14,8 +16,9 @@ const API_URL = "https://ron-swanson-quotes.herokuapp.com"
 app.get("/", async (req, res) => {
     try {
         const response = await axios.get(`${API_URL}/v2/quotes`)
-        console.log(response)
-        res.render("index", { quote: response.data[0] })
+        const date = new Date()
+        const formatDate = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+        res.render("index", { quote: response.data[0], date: formatDate })
     } catch {
         res.send("Erro ao acessar a API")
     }
